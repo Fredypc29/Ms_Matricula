7f7cd858deebb8bee70cd74e5ca444f26a138245
Ronny Panduro <100005222+ronnypanduro@users.noreply.github.com>
2024-01-23 01:04:10 -0500

    Create CursoModel.java

---
+++ Cursos/src/main/java/com/edu/pe/model/CursoModel.java
@@ -0,0 +1,57 @@
+package com.edu.pe.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "curso")
+public class CursoModel {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_curso")
+	private Integer id;
+	
+	private String nombre;
+	
+	private int horas;
+	
+	private int creditos;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getHoras() {
+		return horas;
+	}
+
+	public void setHoras(int horas) {
+		this.horas = horas;
+	}
+
+	public int getCreditos() {
+		return creditos;
+	}
+
+	public void setCreditos(int creditos) {
+		this.creditos = creditos;
+	}
+	
+	
+}
