8dfc8becf6b2cba39dee5eea4f5f0447d40ceb98
Ronny Panduro <100005222+ronnypanduro@users.noreply.github.com>
2024-01-23 01:09:43 -0500

    Create IPagoRepository.java

---
+++ Pago/src/main/java/com/edu/pe/repository/IPagoRepository.java
@@ -0,0 +1,10 @@
+package com.edu.pe.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import com.edu.pe.model.PagoModel;
+
+@Repository
+public interface IPagoRepository extends CrudRepository<PagoModel, Integer> {
+
+}
