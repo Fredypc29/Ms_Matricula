3ba023b7b33fb4bc5976ffd71f898676282f4208
Ronny Panduro <100005222+ronnypanduro@users.noreply.github.com>
2024-01-23 01:09:37 -0500

    Create PagoModel.java

---
+++ Pago/src/main/java/com/edu/pe/model/PagoModel.java
@@ -0,0 +1,56 @@
+package com.edu.pe.model;
+
+import java.util.Date;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="pago")
+public class PagoModel {
+	
+	@Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seqPagos")
+    @SequenceGenerator(name = "seqPagos", allocationSize = 1, sequenceName = "SEQ_PAGOS")
+	@Column(name = "id_pago")
+	private Integer id;
+	
+    @Column(name = "id_matricula")
+    private Integer idMatr;
+    
+	@Column(name = "fecha_pago")
+	private Date fechaMatr;
+	
+	private double monto;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Integer getIdMatr() {
+		return idMatr;
+	}
+
+	public void setIdMatr(Integer idMatr) {
+		this.idMatr = idMatr;
+	}
+
+	public Date getFechaMatr() {
+		return fechaMatr;
+	}
+
+	public void setFechaMatr(Date fechaMatr) {
+		this.fechaMatr = fechaMatr;
+	}
+
+	public double getMonto() {
+		return monto;
+	}
+
+	public void setMonto(double monto) {
+		this.monto = monto;
+	}
+}
